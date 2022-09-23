718422635f8558aa5664f4b668550d58452901f8
yudhakr <deuxyudha@gmail.com>
2022-09-23 18:49:32 +0800

    project menampah responive1

--- asset/script.js
+++ asset/script.js
@@ -1,23 +1,8 @@
-let card = document.querySelectorAll('.card');
-for (cards of card) {
-    cards.addEventListener('mouseover', function (event) {
-        if (event.target.id === "aini") {
-            event.target.setAttribute("src", "assets/image/5aini5new.jpg");
-            return;
-        } else if (event.target.id === "ragam") {
-            event.target.setAttribute("src", "assets/image/9ragam9new.jpg");
-            return;
-        }
-    });
-
-    cards.addEventListener('mouseout', function (event) {
-        if (event.target.id === "aini") {
-            event.target.setAttribute("src", "assets/image/5aini5.jpg");
-            return;
-        }
-        if (event.target.id === "ragam") {
-            event.target.setAttribute("src", "assets/image/9ragam9.jpg");
-            return;
-        }
-    });
+function myFunction(){
+    var x = document.getElementById("myTopnav").value;
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    }else {
+        x.className = "topnav";
+    }
 }
