309948eb2d72439b0dae6976284f88f6a5290bdc
yudhakr <deuxyudha@gmail.com>
2022-09-24 17:35:53 +0800

    responsive 2

--- asset/script.js
+++ asset/script.js
@@ -1,8 +1,23 @@
-function myFunction(){
-    var x = document.getElementById("myTopnav").value;
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    }else {
-        x.className = "topnav";
-    }
+let card = document.querySelectorAll('.card');
+for (cards of card) {
+    cards.addEventListener('mouseover', function (event) {
+        if (event.target.id === "aini") {
+            event.target.setAttribute("src", "asset/img/laptop1.jpeg");
+            return;
+        } else if (event.target.id === "ragam") {
+            event.target.setAttribute("src", "assets/img/lenovo1.jpeg");
+            return;
+        }
+    });
+
+    cards.addEventListener('mouseout', function (event) {
+        if (event.target.id === "aini") {
+            event.target.setAttribute("src", "assets/image/laptop.jpeg");
+            return;
+        }
+        if (event.target.id === "ragam") {
+            event.target.setAttribute("src", "assets/image/lenovo.jpeg");
+            return;
+        }
+    });
 }
